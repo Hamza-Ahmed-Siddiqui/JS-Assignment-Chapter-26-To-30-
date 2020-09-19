34c11e8057deb02dec820162c10f2d883c0a5a21
Hamza-Ahmed-Siddiqui <princehamzaahmed9@gmail.com>
2020-09-19 12:04:38 +0500

    JavaScript Assignment Chapter 26 To 30 Math Methods And Maths Function

---
+++ app.js
@@ -0,0 +1,95 @@
+
+
+        alert("Welocme To My Assignment");
+        alert("Chapter 26 To 30 Complete Assignment !!!");
+
+        // ==================================Date Methods================================================
+
+        // =================Chapter 26 To 30=================
+
+        // ========== Answer#1 =============
+
+        
+        // var roundNum = +prompt("Enter Your Rounded Number Number","3.45214");
+        // alert("Your Number is "+roundNum);
+        // roundNum = Math.round(roundNum);
+        // alert("Your Rounded number is "+roundNum);
+        // roundNum = Math.floor(roundNum);
+        // alert("Your Floor Number is "+roundNum);
+        // roundNum++;
+        // roundNum = Math.ceil(roundNum);
+        // alert("Your Ceil Number is "+roundNum);
+
+        // =========== Answer#2============
+
+        
+        
+        // var roundNum = +prompt("Enter Your Rounded Number Number","3.45214");
+        // alert("Your Number is "+roundNum);
+        // roundNum = Math.round(roundNum);
+        // alert("Your Rounded number is "+roundNum);
+        // roundNum = Math.floor(roundNum);
+        // alert("Your Floor Number is "+roundNum);
+        // roundNum++;
+        // roundNum = Math.ceil(roundNum);
+        // alert("Your Ceil Number is "+roundNum);
+
+
+        // ========== Answer#3=============
+
+        // var absoluteNum = +prompt("Enter Your Absolute Number " , "-4");
+        // alert("Your Number is "+absoluteNum);
+        // var b = absoluteNum*-1;
+        // alert("The absolute value of " +absoluteNum+" is "+b);
+
+
+        // ====== Answer#4 ==============
+
+        // var randomNum = Math.random() ;
+        // var randomNumber =(randomNum*6)+1;
+        // var randomNumbers = Math.round(randomNumber);
+        // // randomNum = Math.ceil();
+        // alert("Your Random Dice value is: "+randomNumbers );
+
+
+        // ===== Answer#5 ======
+
+        // var rrandomNum = Math.ceil(Math.random()*2);
+        // alert("Your Random Dice value is: "+rrandomNum );
+
+        // if(rrandomNum === 1){
+        //     alert("Heads")
+        // }else if(rrandomNum=== 2 ){
+        //     alert("Tails")                         
+        // }else{
+        //     alert("Nothing")
+        // }
+
+        // =========== Answer#6 ===========
+
+        // var randomNumber = Math.random();
+        // var number = (randomNumber*100);
+        // var roundoff = Math.round(number)
+        // alert(roundoff);
+
+
+        // ====== Answer#7 ========
+
+        // var weight  = prompt("Enter Your Weight In Kilograms " , "78 Kilograms");
+        // alert("The weight of user is: "+weight);
+
+
+        // ====== Answer#8 ========
+
+        // var user = +prompt("Enter A Number Between 1 To 10 ","5");
+        // var rrandomNum = Math.ceil(Math.random()*10);
+        // if(user == rrandomNum){
+        //     alert("Congrats: "+rrandomNum );
+        // }else{
+        //     alert("Better luck next time")
+        // }
+
+
+
+
+    // ==================================== Complete Assignment =====================================================    
